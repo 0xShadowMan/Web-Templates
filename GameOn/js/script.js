da9d75110f6b3a123c7ed9b6a87de3cd5a85cc8d
MR.ShadowMan <165802825+0xShadowMan@users.noreply.github.com>
2025-07-17 00:06:12 +0600

    Add files via upload

---
+++ GameOn/js/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
+};
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2400,
+    reset: true
+});
+
+
+sr.reveal('.hero-img',{delay:500, origin: 'top'});
+sr.reveal('.hero h6',{delay:600, origin: 'bottom'});
+sr.reveal('.hero h1',{delay:700, origin: 'bottom'});
+sr.reveal('.hero p',{delay:800, origin: 'top'});
+sr.reveal('.hero h3',{delay:800, origin: 'top'});
+
+sr.reveal('.social a',{delay:1100, origin: 'bottom'});
